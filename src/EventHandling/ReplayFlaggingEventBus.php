8937b203d620a208ec6d08a4054a4c45e4c1a686
Bert Ramakers <bertramakers@me.com>
2017-02-06 14:47:45 +0100

    III-1777: Add an event bus that can flag published messages as replayed.
    It only flags domain messages as “replayed” when publishing a stream during replay mode.
    Replay mode has to be started and stopped manually, the event bus cannot deduce this.

---
+++ src/EventHandling/ReplayFlaggingEventBus.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace CultuurNet\Broadway\EventHandling;
+
+use Broadway\Domain\DomainEventStream;
+use Broadway\Domain\DomainEventStreamInterface;
+use Broadway\Domain\DomainMessage;
+use Broadway\Domain\Metadata;
+use Broadway\EventHandling\EventBusInterface;
+use Broadway\EventHandling\EventListenerInterface;
+use CultuurNet\Broadway\Domain\DomainMessageIsNotReplayed;
+
+class ReplayFlaggingEventBus implements EventBusInterface
+{
+    /**
+     * @var EventBusInterface
+     */
+    private $eventBus;
+
+    /**
+     * @var bool
+     */
+    private $replayMode;
+
+    /**
+     * @param EventBusInterface $eventBus
+     */
+    public function __construct(EventBusInterface $eventBus)
+    {
+        $this->eventBus = $eventBus;
+        $this->replayMode = false;
+    }
+
+    public function startReplayMode()
+    {
+        $this->replayMode = true;
+    }
+
+    public function stopReplayMode()
+    {
+        $this->replayMode = false;
+    }
+
+    /**
+     * @param DomainEventStreamInterface|DomainMessage[] $domainMessages
+     */
+    public function publish(DomainEventStreamInterface $domainMessages)
+    {
+        $replayMetadata = new Metadata(
+            [
+                DomainMessageIsNotReplayed::METADATA_REPLAY_KEY => $this->replayMode,
+            ]
+        );
+
+        $messages = [];
+
+        foreach ($domainMessages as $index => $domainMessage) {
+            $messages[$index] = new DomainMessage(
+                $domainMessage->getId(),
+                $domainMessage->getPlayhead(),
+                $domainMessage->getMetadata()->merge($replayMetadata),
+                $domainMessage->getPayload(),
+                $domainMessage->getRecordedOn()
+            );
+        }
+
+        $stream = new DomainEventStream($messages);
+
+        $this->eventBus->publish($stream);
+    }
+
+    /**
+     * @param EventListenerInterface $eventListener
+     */
+    public function subscribe(EventListenerInterface $eventListener)
+    {
+        $this->eventBus->subscribe($eventListener);
+    }
+}
